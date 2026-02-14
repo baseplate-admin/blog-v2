42779fe395eefd4d3b35eb0c380ae59cb8ab40f8
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-02-14 18:30:32 +0600

    add

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,82 @@
 from django.db import models
+from django import forms
 
-# Create your models here.
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+
+from wagtail.models import Page, Orderable
+from wagtail.fields import RichTextField, StreamField
+from wagtail.blocks import RichTextBlock
+from wagtailcodeblock.blocks import CodeBlock
+from wagtail.images.blocks import ImageChooserBlock
+from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.search import index
+
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        "BlogPage", related_name="tagged_items", on_delete=models.CASCADE
+    )
+
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("intro"),
+    ]
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by("-first_published_at")
+
+        # Filtering by tag
+        tag = request.GET.get("tag")
+        if tag:
+            blogpages = blogpages.filter(blogpage__tags__name=tag)
+
+        context["blogpages"] = blogpages
+        context["request_tag"] = tag
+        return context
+
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = StreamField([
+        ('paragraph', RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('code', CodeBlock(label='Code')),
+    ], use_json_field=True)
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+
+    feed_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    search_fields = Page.search_fields + [
+        index.SearchField("intro"),
+        index.SearchField("body"),
+    ]
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                FieldPanel("date"),
+                FieldPanel("tags"),
+            ],
+            heading="Blog information",
+        ),
+        FieldPanel("intro"),
+        FieldPanel("body"),
+        FieldPanel("feed_image"),
+    ]
+
+    parent_page_types = ["BlogIndexPage"]
+    subpage_types = []
