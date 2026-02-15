797fe0fa736857acc756ca1e6454c99ecd0bfa41
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-02-15 17:11:43 +0600

    add

--- assets/twemoji/index.ts
+++ assets/twemoji/index.ts
@@ -5,13 +5,13 @@ const base = import.meta.env.BASE_URL;
 // Native emoji–like CSS
 const style = document.createElement('style');
 style.textContent = `
-  img.emoji {
-    width: 1em;
-    height: 1em;
-    display: inline-block;
-    vertical-align: -0.1em;
-    pointer-events: none;
-  }
+	img.emoji {
+		width: 1em;
+		height: 1em;
+		display: inline-block;
+		vertical-align: -0.1em;
+		pointer-events: none;
+	}
 `;
 document.head.appendChild(style);
 
