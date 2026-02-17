dc45ef8a5f9b703d12013b30d8a5f3e3e437932e
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-02-17 17:31:26 +0600

    add

--- core/urls.py
+++ core/urls.py
@@ -5,10 +5,11 @@ from django.contrib import admin
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
-
+from wagtail.contrib.sitemaps.views import sitemap
 from search import views as search_views
 
 urlpatterns = [
+    path('sitemap.xml', sitemap),
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
