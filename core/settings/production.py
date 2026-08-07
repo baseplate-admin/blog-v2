c2c05a110068811df5d86f724dbf80a0a2de10cc
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2026-08-07 22:20:28 +0600

    fix: add WhiteNoise immutable file test for Vite-hashed assets

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,4 +1,5 @@
 import os
+import re
 from django.core.exceptions import ImproperlyConfigured
 from .base import *
 
@@ -16,6 +17,13 @@ DJANGO_VITE = {
 # (e.g. after a Wagtail upgrade).
 # See https://docs.djangoproject.com/en/6.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
 STORAGES["staticfiles"]["BACKEND"] = "whitenoise.storage.CompressedStaticFilesStorage"
+
+# WhiteNoise: treat Vite-hashed assets as immutable for max-age caching
+# https://github.com/MrBin99/django-vite#whitenoise
+def _immutable_file_test(path: str, url: str) -> bool:
+    return bool(re.match(r"^.+[.-][0-9a-zA-Z_-]{8,12}\..+$", url))
+
+WHITENOISE_IMMUTABLE_FILE_TEST = _immutable_file_test
 
 SECRET_KEY = "django-insecure-fvpfm@gr_1l8_=%cgx#hk=@*ftymg112p(a5e5m(zx!7)+rs)8"
 
